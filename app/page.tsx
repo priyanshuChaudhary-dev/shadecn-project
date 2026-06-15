bc5f64f5433607bebd7de635e1e4a921a593c782
Priyanshu Chaudhary <priyanshu19825@gmail.com>
2026-06-15 09:51:19 +0530

    feat: add link to Chai page with login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,18 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import ChaiPage from "./chai/page";
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <h1>Chai aur shadecn</h1>
+        <Link href={'http://localhost:3000/chai'}>
+          <Button>Login</Button>
+        </Link>
+
+
       </main>
     </div>
   );
